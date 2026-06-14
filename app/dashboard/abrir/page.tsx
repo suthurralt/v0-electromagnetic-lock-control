1b5a46f9ebb9af8a2c9d100e791b9b8e67329b12
santigarcia8 <santigarcia@itba.edu.ar>
2026-06-14 15:48:39 -0300

    Update page 14/06.tsx

--- app/dashboard/abrir/page.tsx
+++ app/dashboard/abrir/page.tsx
@@ -184,7 +184,8 @@ export default function AbrirLockerPage() {
 
       try {
         const response = await fetch(
-          `http://172.23.72.113/open`,  //ESTA ES LA IP DE LA RASPBERRY, CAMBIAR SI SE USA OTRA
+          `http://172.23.72.113/open`, 
+          `http://192.168.0.97/open`, //ESTA ES LA IP DE LA RASPBERRY, CAMBIAR SI SE USA OTRA
           {
             method: "POST",
             headers: {
